cbb9789f08586ac27e50ec92595be8a7663cf1d3
mihir <vaidyamihir999@gmail.com>
2026-02-23 13:41:34 +0530

    Done

---
+++ Practical3A/minmaxodd.java
@@ -0,0 +1,10 @@
+public class minmaxodd {
+    public static void main(String[] args) {
+
+        int minOdd = 1;
+        int maxOdd = 99;
+
+        System.out.println("Minimum odd number: " + minOdd);
+        System.out.println("Maximum odd number: " + maxOdd);
+    }
+}
